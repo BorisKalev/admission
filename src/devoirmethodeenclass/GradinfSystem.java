a915dde100be6a4926204c37d83b3d78dce18b1e
Boris Kalev <boris_kalev_14@hotmail.com>
2022-11-02 12:03:18 -0400

    adding fileskkkkkk

--- src/devoirmethodeenclass/GradinfSystem.java
+++ src/devoirmethodeenclass/GradinfSystem.java
@@ -16,9 +16,7 @@ import model.Teacher;
  */
 public class GradinfSystem {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
        
